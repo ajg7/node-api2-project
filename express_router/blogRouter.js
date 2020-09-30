9de0eac48239226b70fc853d054e3a03a6d42cf7
ajg7 <ajgebara@gmail.com>
2020-09-30 00:22:19 -0500

    made the get requests and a post request

--- express_router/blogRouter.js
+++ express_router/blogRouter.js
@@ -1,8 +1,62 @@
 const express = require("express");
 const router = express.Router;
-const dataBase = require("../data/db");
+const Database = require("../data/db");
 
 
-router.length("/", (request, response) => {
-    
+router.get("/", (request, response) => {
+    Database.find(request.query)
+        .then(db => {
+            response.status(200).json(dataBase)
+        })
+        .catch(error => {
+            console.log(error);
+            response.status(500).json({
+                message: "The post information could not be retrieved"
+            })
+        })
 })
+
+router.get("/:id", (request, response) => {
+    Database.findById(request.params.id)
+        .then(db => {
+            if(db.length !== 0){
+                response.status(200).json(dataBase)
+            } else {
+                response.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            response.status(500).json({ error: "The post information could not be retrieved" })
+        })
+})
+
+router.get("/:id/comments", (request, response) => {
+    Database.findPostComments(request.params.id)
+        .then(db => {
+            if(!db) {
+                return response.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else {
+                return response.status(200).json(db)
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            response.status(500).json({ error: "The post could not be retrieved" })
+        })
+})
+
+router.post("/", (request, response) => {
+    Database.insert(request.body)
+        .then(db => {
+            response.status(201).json({ message: `A post with id ${db.id} is added to the posts.`})
+        })
+        .catch(error => {
+            console.log(error);
+            response.status(500).json({ error: "There was an error while saving the post to the database" })
+        })
+})
+
+
+
+module.exports = router;
